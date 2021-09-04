f26781ea0e93928b0accaab4f86eecc4a35da19a
Syafiqjos <syafiqjos@gmail.com>
2021-09-04 12:11:42 +0700

    Add coloring and fix and make tidy

--- Intermediate/CalculatorApp/tailwind.config.js
+++ Intermediate/CalculatorApp/tailwind.config.js
@@ -2,6 +2,18 @@ module.exports = {
   // purge: [ 'index.html' ],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    colors: {
+      primary: 'var(--color-primary)',
+      secondary: 'var(--color-secondary)',
+      ternary: 'var(--color-ternary)',
+      neutral1: 'var(--color-neutral1)',
+      neutral2: 'var(--color-neutral2)',
+      neutral3: 'var(--color-neutral3)'
+    },
+    textColor: {
+      primary: 'var(--color-text1)',
+      secondary: 'var(--color-text2)'
+    },
     extend: {
       fontFamily: {
         'spartan': [ '"Spartan"', 'sans-serif' ]
