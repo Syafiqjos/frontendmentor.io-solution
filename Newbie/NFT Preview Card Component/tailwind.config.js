8d17415e5f46ca2e077e1644768fa4793d5ba44c
Syafiqjos <syafiqjos@gmail.com>
2022-01-08 14:21:21 +0700

    Add tailwind basic config of style guide

--- Newbie/NFT Preview Card Component/tailwind.config.js
+++ Newbie/NFT Preview Card Component/tailwind.config.js
@@ -1,7 +1,23 @@
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./index.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary-blue': 'hsl(215, 51%, 70%)',
+        'primary-cyan': 'hsl(178, 100%, 50%)',
+        'neutral-main': 'hsl(217, 54%, 11%)',
+        'neutral-card': 'hsl(216, 50%, 16%)',
+        'neutral-line': 'hsl(215, 32%, 27%)',
+        'neutral-white': 'hsl(0, 0%, 100%)'
+      }
+    },
+    fontFamily: {
+      'outfit': ['"Outfit"', 'sans-serif']
+    },
+    screens: {
+      'mobile': '375px',
+      'desktop': '1440px'
+    }
   },
   plugins: [],
 }
