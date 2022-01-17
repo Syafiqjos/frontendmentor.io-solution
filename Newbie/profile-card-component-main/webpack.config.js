fb3318025cc7c75123d9d44754eeb70c13a253cb
Syafiqjos <syafiqjos@gmail.com>
2022-01-17 13:35:19 +0700

    Install and prepare npm and bootstrap

---
+++ Newbie/profile-card-component-main/webpack.config.js
@@ -0,0 +1,33 @@
+const CopyPlugin = require("copy-webpack-plugin");
+const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    // Creates `style` nodes from JS strings
+                    "style-loader",
+                    // Translates CSS into CommonJS
+                    "css-loader",
+                    // Compiles Sass to CSS
+                    "sass-loader",
+                ],
+            },
+        ]
+    },
+    plugins: [
+        new CopyPlugin({
+            patterns: [
+                { from: "static" }
+            ],
+        }),
+    ]
+};
