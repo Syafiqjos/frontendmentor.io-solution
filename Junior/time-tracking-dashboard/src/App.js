9c4e8ed7f39164d4f2f118520c362c6819ec303f
Syafiqjos <syafiqjos@gmail.com>
2022-01-18 13:56:42 +0700

    Install react router and initiates views and components

--- Junior/time-tracking-dashboard/src/App.js
+++ Junior/time-tracking-dashboard/src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
+
+import Home from './views/Home';
+import Footer from './components/Footer';
 import './App.css';
+
+const route = (
+  <Route path="/" element={<Home />} />
+);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <main>
+          <Routes>
+            {route}
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
---
+++ Junior/time-tracking-dashboard/src/components/Footer.js
@@ -0,0 +1,12 @@
+function Footer() {
+    return (
+        <footer>
+          <div class="attribution">
+            Challenge by <a href="https://www.frontendmentor.io?ref=challenge" rel="noreferrer" target="_blank">Frontend Mentor</a>. 
+            Coded by <a href="https://github.com/syafiqjos">Ahmad Syafiq Aqil Wafi</a>.
+          </div>
+        </footer>
+    );
+}
+
+export default Footer;
