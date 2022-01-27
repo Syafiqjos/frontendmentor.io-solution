8f42a161b14b411ff0dfcfb334e33891efc8132d
Syafiqjos <syafiqjos@gmail.com>
2022-01-27 14:39:00 +0700

    Update base layout

--- Junior/time-tracking-dashboard/src/App.js
+++ Junior/time-tracking-dashboard/src/App.js
@@ -1,29 +1,21 @@
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route
-} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import Home from './views/Home';
-import Footer from './components/Footer';
+import Home from "./views/Home";
+import Footer from "./components/Footer";
 
-const route = (
-  <Route path="/" element={<Home />} />
-);
+const route = <Route path="/" element={<Home />} />;
 
 function App() {
-  return (
-    <Router>
-      <div className="min-h-screen flex flex-col">
-        <main className="grow">
-          <Routes>
-            {route}
-          </Routes>
-        </main>
-        <Footer />
-      </div>
-    </Router>
-  );
+	return (
+		<Router>
+			<div className="min-h-screen flex flex-col">
+				<main className="flex grow items-center justify-center bg-neutral-very-dark-blue">
+					<Routes>{route}</Routes>
+				</main>
+				<Footer />
+			</div>
+		</Router>
+	);
 }
 
 export default App;
--- Junior/time-tracking-dashboard/src/views/Home.js
+++ Junior/time-tracking-dashboard/src/views/Home.js
@@ -1,19 +1,66 @@
-import UserCard from '../components/UserCard';
-import Card from '../components/Card';
+import UserCard from "../components/UserCard";
+import Card from "../components/Card";
 
 function Home() {
-    return (
-        <section className="grid grid-cols-4">
-            <UserCard className="row-span-2" />
-            <Card />
-            <Card />
-            <Card />
+	const timerData = [
+		{
+			backgroundColor: "bg-primary-work",
+			backgroundIcon: "bg-icon-work",
+			title: "Work",
+			time: "32hrs",
+			durationTime: "Last Week",
+			durationDesc: "36hrs",
+		},
+		{
+			backgroundColor: "bg-primary-play",
+			backgroundIcon: "bg-icon-play",
+			title: "Play",
+			time: "10hrs",
+			durationTime: "Last Week",
+			durationDesc: "8hrs",
+		},
+		{
+			backgroundColor: "bg-primary-study",
+			backgroundIcon: "bg-icon-study",
+			title: "Study",
+			time: "4hrs",
+			durationTime: "Last Week",
+			durationDesc: "7hrs",
+		},
+		{
+			backgroundColor: "bg-primary-exercise",
+			backgroundIcon: "bg-icon-exercise",
+			title: "Work",
+			time: "4hrs",
+			durationTime: "Last Week",
+			durationDesc: "5hrs",
+		},
+		{
+			backgroundColor: "bg-primary-social",
+			backgroundIcon: "bg-icon-social",
+			title: "Work",
+			time: "5hrs",
+			durationTime: "Last Week",
+			durationDesc: "10hrs",
+		},
+		{
+			backgroundColor: "bg-primary-self-care",
+			backgroundIcon: "bg-icon-self-care",
+			title: "Work",
+			time: "2hrs",
+			durationTime: "Last Week",
+			durationDesc: "2hrs",
+		},
+	];
 
-            <Card />
-            <Card />
-            <Card />
-        </section>
-    );
+	return (
+		<section className="grid grid-cols-4 h-full gap-5">
+			<UserCard className="row-span-2" />
+			{timerData.map((element) => {
+				return <Card {...element} />;
+			})}
+		</section>
+	);
 }
 
 export default Home;
