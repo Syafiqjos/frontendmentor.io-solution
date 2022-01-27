2043747ba174fe24e55b581fb41dd07238c11c16
Syafiqjos <syafiqjos@gmail.com>
2022-01-27 16:21:49 +0700

    Update footer styling

--- Junior/time-tracking-dashboard/src/components/Footer.js
+++ Junior/time-tracking-dashboard/src/components/Footer.js
@@ -1,12 +1,26 @@
 function Footer() {
-    return (
-        <footer className="grow-0">
-          <div className="attribution">
-            Challenge by <a href="https://www.frontendmentor.io?ref=challenge" rel="noreferrer" target="_blank">Frontend Mentor</a>. 
-            Coded by <a href="https://github.com/syafiqjos">Ahmad Syafiq Aqil Wafi</a>.
-          </div>
-        </footer>
-    );
+	return (
+		<footer className="grow-0">
+			<div className="p-4 text-white bg-primary-social text-center font-rubik font-light">
+				Challenge by{" "}
+				<a
+					href="https://www.frontendmentor.io?ref=challenge"
+					rel="noreferrer"
+					target="_blank"
+				>
+					Frontend Mentor
+				</a>
+				. Coded by{" "}
+				<a
+					className="underline underline-offset-2 decoration-2"
+					href="https://github.com/syafiqjos"
+				>
+					Ahmad Syafiq Aqil Wafi
+				</a>
+				.
+			</div>
+		</footer>
+	);
 }
 
 export default Footer;
